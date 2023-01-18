766ceaab5da55be9f58a04aa4ea33ef548888d3d
Serena Badesha <serenab@apadmi.com>
2023-01-18 11:34:34 +0000

    added .gitignore, fixed title size

--- SatisfactoryCalculator/CalculatorView.swift
+++ SatisfactoryCalculator/CalculatorView.swift
@@ -21,15 +21,18 @@ struct CalculatorView: View {
                     .background(.ultraThinMaterial)
                 Text("Selected Item: \(selectedRecipe)")
                 Spacer()
-                Picker("Select a color", selection: $selectedRecipe) {
-                    let recipeNameRange = 0..<purpleDataVm.recipeNames.count
+                Picker("Select a recipe", selection: $selectedRecipe) {
+                    let recipeNameRange = 0 ..< purpleDataVm.recipeNames.count
                     ForEach(recipeNameRange) { index in
                         Text(purpleDataVm.recipeNames[index])
                     }.pickerStyle(WheelPickerStyle())
+                        .font(.subheadline)
                 }
             }
-            .navigationBarTitle(Text("The Satisfactory Calculator"))
-            .preferredColorScheme(ColorScheme.dark)
+            .navigationBarTitle(Text("Satisfactory Calculator"), displayMode: .inline)
+            .foregroundColor(.orange)
+            
+            //.preferredColorScheme(ColorScheme.dark)
                 
         }
     }
